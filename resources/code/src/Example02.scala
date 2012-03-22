38d4bd807f750674749fe491e24575db991f1ae0
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2012-03-22 13:54:57 +0100

    Add more statements to the example to play with targeted debugging

--- resources/code/src/Example02.scala
+++ resources/code/src/Example02.scala
@@ -1,8 +1,15 @@
 class A {
   def foo[T](a: Int)(b: T): T = b
 
+  def sth() {
+    val another = 12
+  }
+
   def bar {
+    val b: Int = 1
+    val c: String = "abc"
     val a = foo(10) _
     a(2)
+    ()
   }
 }
