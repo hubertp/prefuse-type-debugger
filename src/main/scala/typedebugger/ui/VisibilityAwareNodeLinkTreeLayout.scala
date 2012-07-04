267bb5ae24515aab077a9f004896657b200961f7
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2012-07-04 15:23:45 +0200

    Giving some Scala love to the custom layout manager (previously adapted directly from prefuse sources)

--- src/main/scala/typedebugger/ui/VisibilityAwareNodeLinkTreeLayout.scala
+++ src/main/scala/typedebugger/ui/VisibilityAwareNodeLinkTreeLayout.scala
@@ -2,10 +2,11 @@ package scala.typedebugger
 package ui
 
 import prefuse.data.{Graph, Table, Node, Tuple, Edge, Tree}
-import prefuse.action.layout.graph.NodeLinkTreeLayout
+import prefuse.action.layout.graph.{NodeLinkTreeLayout, Params}
 import prefuse.visual.{VisualItem, NodeItem, EdgeItem}
 
 import scala.collection.JavaConversions._
+import scala.annotation.tailrec
 
 abstract class VisibilityAwareNodeLinkTreeLayout(
   orientation: Int, dspace: Double, bspace: Double, tspace: Double,
@@ -32,9 +33,8 @@ abstract class VisibilityAwareNodeLinkTreeLayout(
     }
   }
   
-  override def getGraph(): Graph = {
+  override def getGraph(): Graph =
     m_vis.getGroup(visibleGroup).asInstanceOf[Graph]
-  }
   
   // Methods below are a direct copy of the original behaviour with the exception
   // that we take into account visible/advanced nodes when calculating
@@ -47,138 +47,196 @@ abstract class VisibilityAwareNodeLinkTreeLayout(
       
     val expanded = n.isExpanded()
     if ( Proxy.getChildCount(n) == 0 || !expanded ) {
-      val l = Proxy.getPreviousSibling(n)
-      np.prelim = if ( l == null ) 0 else (getParams(l).prelim + spacing(l,n,true))
+      np.prelim = Proxy.getPreviousSibling(n) match {
+        case None    => 0
+        case Some(l) => getParams(l).prelim + spacing(l,n,true)
+      }
     } else if ( expanded ) {
       val leftMost = Proxy.getFirstChild(n)
       val rightMost = Proxy.getLastChild(n)
       var defaultAncestor = leftMost
       var c = leftMost
-      var i = 0
-      while (c != null) {
-        firstWalk(c, i, depth+1)
-        defaultAncestor = apportion(c, defaultAncestor)
-        i += 1
-        c = Proxy.getNextSibling(c)
+      
+      @tailrec
+      def firstWalk0(entry: Option[NodeItem], i: Int): Unit = entry match {
+        case Some(c0) => 
+          firstWalk(c0, i, depth+1)
+          defaultAncestor = apportionVisible(c0, defaultAncestor.get)
+          firstWalk0(Proxy.getNextSibling(c0), i + 1)
+        case None     =>
       }
+      firstWalk0(c, 0)
           
       executeShifts(n)
           
       val midpoint = 0.5 *
          (getParams(leftMost).prelim + getParams(rightMost).prelim)
           
-      val left = Proxy.getPreviousSibling(n)
-      if ( left != null ) {
-        np.prelim = getParams(left).prelim + spacing(left, n, true)
-        np.mod = np.prelim - midpoint
-      } else {
-        np.prelim = midpoint
+      Proxy.getPreviousSibling(n) match {
+        case Some(left) =>
+          np.prelim = getParams(left).prelim + spacing(left, n, true)
+          np.mod = np.prelim - midpoint
+        case None       =>
+          np.prelim = midpoint
       }
     }
   }
   
-  override def apportion(v: NodeItem, a: NodeItem): NodeItem = {        
-    val w = Proxy.getPreviousSibling(v)
-    if ( w != null ) {
-      var vip, vim, vop, vom: NodeItem = null
-      var sip, sim, sop, som: Double = 0
-
-      vip = v; vop = v
-
-      vim = w
-      vom = Proxy.getFirstChild(vip.getParent().asInstanceOf[NodeItem])
-
-      sip = getParams(vip).mod
-      sop = getParams(vop).mod
-      sim = getParams(vim).mod
-      som = getParams(vom).mod
-      
-      var nr = nextRight(vim) // TODO: need to use our version
-      var nl = nextLeft(vip)  // same here
-      while ( nr != null && nl != null ) {
-        vim = nr
-        vip = nl
-        vom = nextLeft(vom)
-        vop = nextRight(vop)
-        getParams(vop).ancestor = v
-        val shift: Double = (getParams(vim).prelim + sim) - 
-            (getParams(vip).prelim + sip) + spacing(vim,vip,false)
-        if ( shift > 0 ) {
-            moveSubtree(ancestor(vim,v,a), v, shift)
-            sip += shift
-            sop += shift
-        }
-        sim += getParams(vim).mod
-        sip += getParams(vip).mod
-        som += getParams(vom).mod
-        sop += getParams(vop).mod
+  override def apportion(v: NodeItem, a: NodeItem): NodeItem =
+    throw new Exception("disallowed")
+  
+  def apportionVisible(v: NodeItem, a: NodeItem): Option[NodeItem] = {        
+    Proxy.getPreviousSibling(v) match {
+      case sibl@Some(w) =>
+        var sip, sim, sop, som: Double = 0
+  
+        var vip: Option[NodeItem] = Some(v)
+        var vop: Option[NodeItem] = Some(v)
+        var vim: Option[NodeItem] = sibl
+        var vom = Proxy.getFirstChild(v.getParent().asInstanceOf[NodeItem])
+  
+        sip = getParams(vip.get).mod
+        sop = getParams(vop.get).mod
+        sim = getParams(vim.get).mod
+        som = getParams(vom.get).mod
         
-        nr = nextRight(vim)
-        nl = nextLeft(vip)
-      }
-      if ( nr != null && nextRight(vop) == null ) {
-        val vopp = getParams(vop)
-        vopp.thread = nr
-        vopp.mod += sim - sop
-      }
-      if ( nl != null && nextLeft(vom) == null ) {
-        val vomp = getParams(vom)
-        vomp.thread = nl
-        vomp.mod += sip - som
-        v
-      } else a
-    } else a
+        @tailrec
+        def apportion0(nr0: Option[NodeItem], nl0: Option[NodeItem]): (Option[NodeItem], Option[NodeItem]) = (nr0, nl0) match {
+          case (Some(nr1), Some(nl1)) =>
+            vim = nr0
+            vip = nl0
+            vom = nextVisibleLeft(vom)
+            vop = nextVisibleRight(vop)
+            getParams(vop).ancestor = v
+            val shift: Double = (getParams(vim).prelim + sim) - 
+              (getParams(vip).prelim + sip) + spacing(vim.get, vip.get, false)
+            if ( shift > 0 ) {
+              moveSubtree(ancestor(vim,v,a), v, shift)
+              sip += shift
+              sop += shift
+            }
+            sim += getParams(vim).mod
+            sip += getParams(vip).mod
+            som += getParams(vom).mod
+            sop += getParams(vop).mod
+            apportion0(nextVisibleRight(vim), nextVisibleLeft(vip))
+          case res =>
+            res
+        }
+        val (nr, nl) = apportion0(nextVisibleRight(vim), nextVisibleLeft(vip))
+        
+        (nr, nextVisibleRight(vop)) match {
+          case (Some(nr1), None) =>
+            val vopp = getParams(vop)
+            vopp.thread = nr1
+            vopp.mod += sim - sop
+          case _ =>
+            ()
+        }
+        (nl, nextVisibleLeft(vom)) match {
+          case (Some(nl1), None) =>
+            val vomp = getParams(vom)
+            vomp.thread = nl1
+            vomp.mod += sip - som
+            Some(v)
+          case _ =>
+            Some(a)
+        }
+      case None =>
+        assert(a != null)
+        Some(a)
+    }
   }
   
-  override def nextLeft(n: NodeItem): NodeItem = {
-    var c: NodeItem = null
-    if ( n.isExpanded() && Proxy.isValid(n) ) c = Proxy.getFirstChild(n)
-    if (c != null) c else getParams(n).thread
+  override def nextLeft(n: NodeItem): NodeItem =
+    throw new Exception("dissallowed")
+  
+  override def nextRight(n: NodeItem): NodeItem =
+    throw new Exception("dissallowed")
+  
+  def nextVisibleLeft(n: Option[NodeItem]): Option[NodeItem] =
+    nextVisible(n, Proxy.getFirstChild _)
+  
+  def nextVisibleRight(n: Option[NodeItem]): Option[NodeItem] =
+    nextVisible(n, Proxy.getLastChild _)
+    
+  def nextVisible(n: Option[NodeItem], getChild: NodeItem => Option[NodeItem]): Option[NodeItem] = {
+    def noChild(n0: NodeItem) = {
+      val tmp1 = getParams(n0).thread
+      if (tmp1 != null) // shouldn't this be a precondition?
+        Some(tmp1)
+      else
+        None
+    }
+
+    n match {
+      case Some(n1) =>
+        if ( n1.isExpanded() && Proxy.isValid(n1))
+          getChild(n1) match {
+            case None => noChild(n1)
+            case next => next
+          }
+        else noChild(n1)
+      case _        =>
+        throw new Exception("Invalid operation: nextVisible")
+        n
+    }
   }
   
-  override def nextRight(n: NodeItem): NodeItem = {
-    var c: NodeItem = null
-    if ( n.isExpanded() && Proxy.isValid(n) ) c = Proxy.getLastChild(n)
-    if (c != null) c else getParams(n).thread
+  override def getParams(item: NodeItem): Params =
+    if (item != null) getParams(Some(item)) else (throw new Exception("Invalid operation: getParams"))
+    
+  def getParams(item: Option[NodeItem]): Params = item match {
+    case Some(item0) => super.getParams(item0)
+    case None        => throw new Exception("Invalid operation")
   }
   
   override def executeShifts(n: NodeItem) {
-    var shift, change: Double = 0
-    var c = Proxy.getLastChild(n)
-    while (c != null) {
-      val cp = getParams(c)
-      cp.prelim += shift
-      cp.mod += shift
-      change += cp.change
-      shift += cp.shift + change
-      c = Proxy.getPreviousSibling(c)
+    @tailrec
+    def executeShifts0(c0: Option[NodeItem], shift: Double, change: Double): Unit = c0 match {
+      case Some(c1) =>
+        val cp = getParams(c1)
+        cp.prelim += shift
+        cp.mod += shift
+        val change1 = change + cp.change
+        executeShifts0(Proxy.getPreviousSibling(c1), cp.shift + change1, change1)
+      case None     =>
     }
+    executeShifts0(Proxy.getLastChild(n), 0, 0)
   }
 
-  override def ancestor(vim: NodeItem, v: NodeItem, a: NodeItem): NodeItem = {
+  override def ancestor(vim0: NodeItem, v: NodeItem, a: NodeItem): NodeItem =
+    throw new Exception("disallowed")
+    
+  def ancestor(vim0: Option[NodeItem], v: NodeItem, a: NodeItem): NodeItem = {
     val p:NodeItem = v.getParent().asInstanceOf[NodeItem]
-    val vimp = getParams(vim)
-    if (vimp.ancestor.getParent() == p) vimp.ancestor else a
+    vim0 match {
+      case Some(vim) =>
+        val vimp = getParams(vim)
+        if (vimp.ancestor.getParent() == p) vimp.ancestor else a
+      case None       =>
+        a
+    }
   }
   
-  override def secondWalk(n: NodeItem, p: NodeItem, m: Double, depth0: Int) {
+  
+  override def secondWalk(n: NodeItem, p: NodeItem, m: Double, depth: Int) {
     val np = getParams(n)
-    var depth = depth0
+
     setBreadth(n, p, np.prelim + m)
     setDepth(n, p, m_depths(depth))
     
-    if ( n.isExpanded() && Proxy.isValid(n) ) {
-      depth += 1
-      var c = Proxy.getFirstChild(n)
-      while (c != null) {
-        secondWalk(c, n, m + np.mod, depth)
-        c = Proxy.getNextSibling(c)
-      }
+    @tailrec
+    def secondWalk0(c0: Option[NodeItem], depth0: Int): Unit = c0 match {
+      case Some(c1) =>
+        secondWalk(c1, n, m + np.mod, depth0)
+        secondWalk0(Proxy.getNextSibling(c1), depth0)
+      case None     =>
     }
+    if (n.isExpanded() && Proxy.isValid(n))
+      secondWalk0(Proxy.getFirstChild(n), depth + 1)
     np.clear()
   }
-  
-  //end of custom layout, specialized NodeLinkTreeLayout for our needs
   
   // Provide main custom operation for navigating between nodes, 
   // that takes into account visibility requirements of the type debugger
@@ -189,32 +247,28 @@ abstract class VisibilityAwareNodeLinkTreeLayout(
     def getChildCount(underlying: NodeItem): Int =
       underlying.children_[NodeItem].toList.filter(isValid).size
     
-    def getFirstChild(underlying: NodeItem): NodeItem = {
-      val first = underlying.getFirstChild().asInstanceOf[NodeItem]
-      if (first != null) {
-        if (!isValid(first)) Proxy.getNextSibling(first) else first
-      } else null
+    
+    private def getChild(initial: NodeItem, backtrack: NodeItem => Option[NodeItem]): Option[NodeItem] =
+      if (initial != null) if (!isValid(initial)) backtrack(initial) else Some(initial)
+      else None
+        
+    def getFirstChild(underlying: NodeItem) =
+      getChild(underlying.getFirstChild().asInstanceOf[NodeItem], Proxy.getNextSibling _)
+    
+    def getLastChild(underlying: NodeItem) =
+      getChild(underlying.getLastChild().asInstanceOf[NodeItem], Proxy.getPreviousSibling _)
+    
+    private def getSibling(underlying: NodeItem, which: NodeItem => NodeItem): Option[NodeItem] = {
+      var found = which(underlying)
+      while (found != null && !isValid(found))
+        found = which(found)
+      if (found == null) None else Some(found)
     }
     
-    def getLastChild(underlying: NodeItem): NodeItem = {
-      val last = underlying.getLastChild().asInstanceOf[NodeItem]
-      if (last != null) {
-        if (!isValid(last)) Proxy.getPreviousSibling(last) else last
-      } else null
-    }
+    def getPreviousSibling(underlying: NodeItem) =
+      getSibling(underlying, _.getPreviousSibling().asInstanceOf[NodeItem])
     
-    def getPreviousSibling(underlying: NodeItem): NodeItem = {
-      var previous = underlying.getPreviousSibling().asInstanceOf[NodeItem]
-      while (previous != null && !isValid(previous))
-        previous = previous.getPreviousSibling().asInstanceOf[NodeItem]
-      previous
-    }
-    
-    def getNextSibling(underlying: NodeItem): NodeItem = {
-      var next = underlying.getNextSibling().asInstanceOf[NodeItem]
-      while (next != null && !isValid(next))
-        next = next.getNextSibling().asInstanceOf[NodeItem]
-      next
-    }
+    def getNextSibling(underlying: NodeItem) =
+      getSibling(underlying, _.getNextSibling().asInstanceOf[NodeItem])
   }
 }
